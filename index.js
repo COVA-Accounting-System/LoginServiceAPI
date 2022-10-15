901f328e4f2c18ccbd401dde7cd88b404aa12ca8
jacovzap <jacovzap@gmail.com>
2022-10-14 21:26:31 -0400

    version deployed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import app from "./src/config/server.config.js";
+import { config } from "dotenv";
+config();
 
 app.set(`port`, process.env.PORT);
-app.listen(app.get(`port`));
+app.listen(app.get(`port`), "0.0.0.0");
 console.log(`Server on port ${app.get(`port`)}`);
--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
 
 try {
   const db = await mongoose.connect(
-    `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@authentication-api.6mso7s4.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@acsys.whd9p61.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority&ssl=true`
+    // `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-zzbedaa-shard-00-00.whd9p61.mongodb.net:27017,ac-zzbedaa-shard-00-01.whd9p61.mongodb.net:27017,ac-zzbedaa-shard-00-02.whd9p61.mongodb.net:27017/${process.env.DATABASE_NAME}?ssl=true&replicaSet=atlas-fkmmc5-shard-0&authSource=admin&retryWrites=true&w=majority`
   );
   console.log(`Database is connected to ${db.connection.name}`);
 } catch (error) {
