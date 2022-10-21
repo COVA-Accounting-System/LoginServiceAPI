b0e6bce943d648077285e085aaec08fa0b32d9e9
jacovzap <jacovzap@gmail.com>
2022-10-21 09:30:01 -0400

    corrections

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -5,7 +5,7 @@ config();
 try {
   const db = await mongoose.connect(
     `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@acsys.whd9p61.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority&ssl=true`
-    // `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-zzbedaa-shard-00-00.whd9p61.mongodb.net:27017,ac-zzbedaa-shard-00-01.whd9p61.mongodb.net:27017,ac-zzbedaa-shard-00-02.whd9p61.mongodb.net:27017/${process.env.DATABASE_NAME}?ssl=true&replicaSet=atlas-fkmmc5-shard-0&authSource=admin&retryWrites=true&w=majority`
+     // `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-zzbedaa-shard-00-00.whd9p61.mongodb.net:27017,ac-zzbedaa-shard-00-01.whd9p61.mongodb.net:27017,ac-zzbedaa-shard-00-02.whd9p61.mongodb.net:27017/${process.env.DATABASE_NAME}?ssl=true&replicaSet=atlas-fkmmc5-shard-0&authSource=admin&retryWrites=true&w=majority`
   );
   console.log(`Database is connected to ${db.connection.name}`);
 } catch (error) {
